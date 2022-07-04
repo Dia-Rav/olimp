51356eaed873759a890bbf2f82e0c86ee53133e7
Диана <ravilova.dm@phystech.edu>
2022-07-04 14:04:03 +0300

    подгонка шрифта, разные типы шрифтов

--- text_from_excel.py
+++ text_from_excel.py
@@ -3,8 +3,15 @@ import pandas as pd
 
 mebers_com = {}
 list_com = pd.read_excel('list of comands.xlsx')
-font = ImageFont.truetype("C:\Windows\Fonts\Gabriola.ttf", 20)
-coor = [(130, 110), (410, 110), (130, 310), (410, 310), (130, 500), (410, 490), (130, 675)]
+font_text = ImageFont.truetype("C:\Windows\Fonts\BOOKOSI.TTF", 23)
+font_for_head = ImageFont.truetype("C:\Windows\Fonts\BOOKOSB.TTF", 20)
+coor_head = [(130, 110), (410, 110), (130, 310), (410, 310), (130, 500), (410, 490), (130, 675)]
+coor = [(130, 150), (410, 150), (130, 350), (410, 350), (130, 550), (410, 530), (130, 715)]
+max_size = 190
+max_size_head = 160
+def check_len (text, font):
+    text = text.split('\n')
+    return  max(list(map(lambda x: font.getlength(x), text)))
 
 for data in list_com.itertuples(index=False, name='People'):
     com = data.Команда
@@ -17,12 +24,34 @@ for data in list_com.itertuples(index=False, name='People'):
         mebers_com [com] = 0 
         image = Image.open("template.png")
         drawer = ImageDraw.Draw(image)
+
     if data.Статус != "Руководитель":
-        text = "{}\n\"{}\"\n{}".format (data.Статус, com, data.ФИО)
+        text_head = "{}\n\"{}\"".format (data.Статус, com)
+        text = "{}".format (data.ФИО)
     else:
         new_fio = data.ФИО.split()
         new_fio = new_fio[0] + '\n' + new_fio[1] + ' ' + new_fio[2]
-        text = "{}\n\"{}\"\n{}".format (data.Статус, com, new_fio)
-    drawer.multiline_text(coor[mebers_com [com]], text, font=font, align = "center", anchor="ms", fill='black')
+        text_head = "{}\n\"{}\"".format (data.Статус, com)
+        text = "{}".format (new_fio)
+
+    if check_len(text, font_text) > max_size:
+        new_font = ImageFont.truetype("C:\Windows\Fonts\BOOKOSI.TTF", 23)
+        new_font = new_font.font_variant(size = new_font.size-1)
+        while check_len(text, new_font) > max_size and new_font.size > 1 :
+            new_font= new_font.font_variant(size = new_font.size-1)
+        drawer.multiline_text(coor[mebers_com [com]], text, font = new_font, align = "center", anchor="ms", fill='black')
+    else:
+        drawer.multiline_text(coor[mebers_com [com]], text, font = font_text, align = "center", anchor="ms", fill='black')
+
+    if check_len(text_head, font_for_head) > max_size:
+        new_font = ImageFont.truetype("C:\Windows\Fonts\BOOKOSB.TTF", 20)
+        new_font = new_font.font_variant(size = new_font.size-1)
+        while check_len(text_head, new_font) > max_size_head and new_font.size > 1 :
+            new_font= new_font.font_variant(size = new_font.size-1)
+        drawer.multiline_text(coor_head[mebers_com [com]], text_head, font = new_font, align = "center", anchor="ms", fill='black')
+    else:
+        drawer.multiline_text(coor_head[mebers_com [com]], text_head, font = font_for_head, align = "center", anchor="ms", fill='black')
     image.save(name_file)
+
+
 

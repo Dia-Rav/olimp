ab61cd9d5b0d84a2a3779a02af8a6e0ccc104f59
Dia-Rav <90519131+Dia-Rav@users.noreply.github.com>
2022-07-15 12:30:45 +0300

    Add files via upload

--- text_from_excel.py
+++ text_from_excel.py
@@ -3,55 +3,66 @@ import pandas as pd
 
 mebers_com = {}
 list_com = pd.read_excel('list of comands.xlsx')
-font_text = ImageFont.truetype("C:\Windows\Fonts\BOOKOSI.TTF", 23)
-font_for_head = ImageFont.truetype("C:\Windows\Fonts\BOOKOSB.TTF", 20)
-coor_head = [(130, 110), (410, 110), (130, 310), (410, 310), (130, 500), (410, 490), (130, 675)]
-coor = [(130, 150), (410, 150), (130, 350), (410, 350), (130, 550), (410, 530), (130, 715)]
-max_size = 190
-max_size_head = 160
+font_text = ImageFont.truetype("arial.ttf", 100)
+font_for_head = ImageFont.truetype("arial.ttf", 140)
+coor_head = [(1000, 470), (2353, 470), (1000, 1271), (2353, 1271), (1000, 2080), (2353, 2080),(1000, 470), (2353, 470), (1000, 1271), (2353, 1271), (1000, 2080), (2353, 2080) ]
+coor = [(1000, 600), (2350, 600), (1000, 1400), (2353, 1400), (1000, 2220), (2353, 2220),(1000, 600), (2350, 600), (1000, 1400), (2353, 1400), (1000, 2220), (2353, 2220)]
+max_size = 1200
+max_size_head = 1200
 def check_len (text, font):
     text = text.split('\n')
     return  max(list(map(lambda x: font.getlength(x), text)))
 
 for data in list_com.itertuples(index=False, name='People'):
     com = data.Команда
-    name_file = com + ".png"
-    if com in mebers_com:
+
+    x = len(com.split())
+    if com in mebers_com and mebers_com [com]> 4:
+        name_file = com + "(1) .png"
+    else:
+        name_file =  com + ".png"
+
+    if com in mebers_com and mebers_com [com] != 5:
         mebers_com [com] += 1
         image = Image.open(name_file)
         drawer = ImageDraw.Draw(image)
-    else: 
-        mebers_com [com] = 0 
+    elif com in mebers_com  and  mebers_com [com] == 5: 
+        mebers_com [com] += 1
+        image = Image.open("template.png")
+        drawer = ImageDraw.Draw(image)
+    else:
+        mebers_com [com] = 0
         image = Image.open("template.png")
         drawer = ImageDraw.Draw(image)
 
     if data.Статус != "Руководитель":
-        text_head = "{}\n\"{}\"".format (data.Статус, com)
+        text_head = "{}\n".format (data.Статус)
         fio = data.ФИО.split()
-        text = "{} {}".format (fio[0], fio[1])
+        font_text = ImageFont.truetype("arial.ttf", 90)
+        text = "\"{}\"\n{} {}".format (com, fio[0], fio[1])
     else:
         new_fio = data.ФИО.split()
         new_fio = new_fio[0] + '\n' + new_fio[1] + ' ' + new_fio[2]
-        text_head = "{}\n\"{}\"".format (data.Статус, com)
-        text = "{}".format (new_fio)
+        text_head = "{}\n".format (data.Статус)
+        text = "\"{}\"\n{}".format (com, new_fio)
 
     if check_len(text, font_text) > max_size:
-        new_font = ImageFont.truetype("C:\Windows\Fonts\BOOKOSI.TTF", 23)
+        new_font = ImageFont.truetype("arial.ttf", 140)
         new_font = new_font.font_variant(size = new_font.size-1)
         while check_len(text, new_font) > max_size and new_font.size > 1 :
             new_font= new_font.font_variant(size = new_font.size-1)
-        drawer.multiline_text(coor[mebers_com [com]], text, font = new_font, align = "center", anchor="ms", fill='black')
+        drawer.multiline_text(coor[mebers_com [com]-1], text, font = new_font, align = "center", anchor="ms", spacing=10, fill='black')
     else:
-        drawer.multiline_text(coor[mebers_com [com]], text, font = font_text, align = "center", anchor="ms", fill='black')
+        drawer.multiline_text(coor[mebers_com [com]-1], text, font = font_text, align = "center", anchor="ms", spacing=10, fill='black')
 
     if check_len(text_head, font_for_head) > max_size:
-        new_font = ImageFont.truetype("C:\Windows\Fonts\BOOKOSB.TTF", 20)
+        new_font = ImageFont.truetype("arial.ttf", 140)
         new_font = new_font.font_variant(size = new_font.size-1)
         while check_len(text_head, new_font) > max_size_head and new_font.size > 1 :
             new_font= new_font.font_variant(size = new_font.size-1)
-        drawer.multiline_text(coor_head[mebers_com [com]], text_head, font = new_font, align = "center", anchor="ms", fill='black')
+        drawer.multiline_text(coor_head[mebers_com [com]-1], text_head, font = new_font, align = "center", anchor="ms", spacing=10, fill='black')
     else:
-        drawer.multiline_text(coor_head[mebers_com [com]], text_head, font = font_for_head, align = "center", anchor="ms", fill='black')
+        drawer.multiline_text(coor_head[mebers_com [com]-1], text_head, font = font_for_head, align = "center", anchor="ms", spacing=10, fill='black')
     image.save(name_file)
 
 
--- text_on_image.py
+++ text_on_image.py
@@ -1,21 +1,62 @@
+from operator import ne
 from PIL import Image, ImageDraw, ImageFont
-comands = open ("comands.txt", "r", encoding='utf-8')
-font = ImageFont.truetype("arial.ttf", 20)
+import pandas as pd
 
-for s in comands:
-    image = Image.open("template.png")
-    drawer = ImageDraw.Draw(image)
-    com = s[:-1]
-    text = "Участник\n\"{}\"".format (com)
-    drawer.multiline_text((130, 120), text, font=font, align = "center", anchor="ms", fill='black')
-    drawer.multiline_text((410, 120), text, font=font, align = "center", anchor="ms", fill='black')
-    drawer.multiline_text((130, 320), text, font=font, align = "center", anchor="ms", fill='black')
-    drawer.multiline_text((410, 320), text, font=font, align = "center", anchor="ms", fill='black')
-    drawer.multiline_text((130, 500), text, font=font, align = "center", anchor="ms", fill='black')
-    drawer.multiline_text((410, 500), text, font=font, align = "center", anchor="ms", fill='black')
-    text = "Руководитель\n\"{}\"".format (com)
-    drawer.multiline_text((130, 695), text, font=font, align = "center", anchor="ms", fill='black')
-    name_file = com + ".png"
-    print (name_file)
+mebers_com = {}
+list_com = pd.read_excel('list_other.xlsx')
+font_text = ImageFont.truetype("arial.ttf", 100)
+font_for_head = ImageFont.truetype("arial.ttf", 140)
+coor_head = [(1000, 470), (2353, 470), (1000, 1271), (2353, 1271), (1000, 2080), (2353, 2080),(1000, 470), (2353, 470), (1000, 1271), (2353, 1271), (1000, 2080), (2353, 2080), (1000, 470), (2353, 470), (1000, 1271), (2353, 1271), (1000, 2080), (2353, 2080) ]
+coor = [(1000, 600), (2350, 600), (1000, 1400), (2353, 1400), (1000, 2220), (2353, 2220),(1000, 600), (2350, 600), (1000, 1400), (2353, 1400), (1000, 2220), (2353, 2220), (1000, 600), (2350, 600), (1000, 1400), (2353, 1400), (1000, 2220), (2353, 2220)]
+max_size = 1200
+max_size_head = 1200
+def check_len (text, font):
+    text = text.split('\n')
+    return  max(list(map(lambda x: font.getlength(x), text)))
+
+for data in list_com.itertuples(index=False, name='People'):
+    com = data.Команда
+    if com in mebers_com and mebers_com [com] > 5:
+        name_file = com + "({}).png".format((mebers_com [com]|+1)//6)
+    else:
+        name_file =  com + ".png"
+    if  com in mebers_com:
+        mebers_com [com] += 1
+    else:
+        mebers_com [com] = 1
+    if mebers_com [com] % 6 != 1 and mebers_com [com] != 1:
+        image = Image.open(name_file)
+        drawer = ImageDraw.Draw(image)
+    elif mebers_com [com]%6 == 1 and mebers_com [com] != 1: 
+        image = Image.open("template.png")
+        drawer = ImageDraw.Draw(image)
+    else:
+        image = Image.open("template.png")
+        drawer = ImageDraw.Draw(image)
+
+    new_fio = data.ФИО.split()
+    if len(new_fio)>2:
+        new_fio = str(new_fio[0] + '\n' + new_fio[1] + ' ' + new_fio[2])
+    else:    
+        new_fio = str(new_fio[0] + ' ' + new_fio[1])
+    text_head = "{}\n".format (data.Статус)
+    text = "{}".format (new_fio)
+
+    if check_len(text, font_text) > max_size:
+        new_font = ImageFont.truetype("arial.ttf", 140)
+        new_font = new_font.font_variant(size = new_font.size-1)
+        while check_len(text, new_font) > max_size and new_font.size > 1 :
+            new_font= new_font.font_variant(size = new_font.size-1)
+        drawer.multiline_text(coor[mebers_com [com]%6], text, font = new_font, align = "center", anchor="ms", spacing=10, fill='black')
+    else:
+        drawer.multiline_text(coor[mebers_com [com]%6], text, font = font_text, align = "center", anchor="ms", spacing=10, fill='black')
+
+    if check_len(text_head, font_for_head) > max_size:
+        new_font = ImageFont.truetype("arial.ttf", 140)
+        new_font = new_font.font_variant(size = new_font.size-1)
+        while check_len(text_head, new_font) > max_size_head and new_font.size > 1 :
+            new_font= new_font.font_variant(size = new_font.size-1)
+        drawer.multiline_text(coor_head[mebers_com [com]%6], text_head, font = new_font, align = "center", anchor="ms", spacing=10, fill='black')
+    else:
+        drawer.multiline_text(coor_head[mebers_com [com]%6], text_head, font = font_for_head, align = "center", anchor="ms", spacing=10, fill='black')
     image.save(name_file)
-
